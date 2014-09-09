96f57d10e03e4610ad1c6e511631720fafafcdc4
a.zhou <a.zhou@us.nature.com>
2014-09-09 15:13:33 -0400

    first commit

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,69 @@
+package org.example;
+ 
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+ 
+import java.util.Set;
+ 
+@Entity
+@XmlRootElement
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+ 
+    @Id
+    private long id;
+ 
+    @Column(name="FIRST_NAME")
+    private String firstName;
+ 
+    @Column(name="LAST_NAME")
+    private String lastName;
+ 
+    @OneToOne(mappedBy="customer", cascade={CascadeType.ALL})
+    private Address address;
+ 
+    @OneToMany(mappedBy="customer", cascade={CascadeType.ALL})
+    private Set<PhoneNumber> phoneNumbers;
+ 
+    public long getId() {
+        return this.id;
+    }
+ 
+    public void setId(long id) {
+        this.id = id;
+    }
+ 
+    public String getFirstName() {
+        return this.firstName;
+    }
+ 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+ 
+    public String getLastName() {
+        return this.lastName;
+    }
+ 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+ 
+    public Address getAddress() {
+        return this.address;
+    }
+ 
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+ 
+    public Set<PhoneNumber> getPhoneNumbers() {
+        return this.phoneNumbers;
+    }
+ 
+    public void setPhoneNumbers(Set<PhoneNumber> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+ 
+}
